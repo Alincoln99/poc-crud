2de668d56e0d1bb53805db4b79a6a1388a2947ff
Wilfried <komnouemsi@gmail.com>
2019-11-04 21:15:52 +0100

    first commit

---
+++ src/main/java/com/o7planning/FormCrudSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.o7planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormCrudSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormCrudSpringMvcApplication.class, args);
+	}
+
+}
